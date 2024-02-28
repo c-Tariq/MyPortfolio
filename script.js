8c8d91598e9b4edc8b98ca8f545c6b9e6ff6ecb5
Tariq <t4riq.cs@gmail.com>
2024-02-29 01:05:54 +0300

    Finished

---
+++ script.js
@@ -0,0 +1,44 @@
+    
+//for opening skils, education tab
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-content");
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+
+
+var sidemenu = document.getElementById("sidemenu");
+
+function openmenu(){
+    sidemenu.style.right = "0";
+
+}
+function closemenu(){
+    sidemenu.style.right = "-200px";
+    console.log("gg")
+}
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzhqM-8NQnaePDlyIHXYUiXUvZO8FbfvHRzXZUETyjlnhPitcYIZOqJvUML0AC1jT4/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg")
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+        msg.innerHTML = "Message sent successfully!"
+        setTimeout(function(){
+            msg.innerHTML = ""
+        },5000)
+        form.reset()
+    })
+    .catch(error => console.error('Error!', error.message))
+})
